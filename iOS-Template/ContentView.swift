f7692035f844ece318589c01a8696e55476e1aa0
Shawn <>
2024-06-07 21:28:55 +0800

    Create project

---
+++ iOS-Template/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Template
+//
+//  Created by 陈荣雄 on 2024/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Template/iOS_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TemplateApp.swift
+//  iOS-Template
+//
+//  Created by 陈荣雄 on 2024/6/7.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
